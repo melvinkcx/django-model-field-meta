06c950b5ef249e6bda7e2d61e394366a9db62f49
melvinkcx <melvinkcx@gmail.com>
2019-12-05 10:14:09 +0900

    Added has_field_meta(); Updated test cases

--- model_field_meta/mixins.py
+++ model_field_meta/mixins.py
@@ -2,3 +2,8 @@ class FieldMetaMixin:
     @classmethod
     def get_field_meta(cls, field_name):
         return cls._meta.get_field(field_name).get_meta()
+
+    @classmethod
+    def has_field_meta(cls, field_name):
+        return hasattr(cls._meta.get_field(field_name), "_meta") \
+               and cls.get_field_meta(field_name) is not None
--- tests/mixins.py
+++ tests/mixins.py
@@ -4,11 +4,28 @@ from tests.models import Coffee
 
 
 class FieldMetaMixinTestCase(TestCase):
+    def setUp(self):
+        Coffee.objects.all().delete()
+
+    def _create_coffee(self):
+        return Coffee.objects.create(brand="Malongo", blend="Arabica", packaging="0.5kg")
+
     def test_instance_get_meta(self):
-        malongo_arabica = Coffee.objects.create(brand="Malongo", blend="Arabica", packaging="0.5kg")
+        malongo_arabica = self._create_coffee()
         meta = malongo_arabica.get_field_meta("packaging")
         self.assertDictEqual(meta, Coffee.packaging_meta)
 
     def test_class_get_meta(self):
         meta = Coffee.get_field_meta("packaging")
         self.assertDictEqual(meta, Coffee.packaging_meta)
+
+    def test_instance_has_meta(self):
+        malongo_arabica = self._create_coffee()
+        self.assertFalse(malongo_arabica.has_field_meta("brand"))
+        self.assertFalse(malongo_arabica.has_field_meta("blend"))
+        self.assertTrue(malongo_arabica.has_field_meta("packaging"))
+
+    def test_class_has_meta(self):
+        self.assertFalse(Coffee.has_field_meta("brand"))
+        self.assertFalse(Coffee.has_field_meta("blend"))
+        self.assertTrue(Coffee.has_field_meta("packaging"))
