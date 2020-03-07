dfc3a3c2b04cb246f978e7b04261578cd7db309b
melvinkcx <melvinkcx@gmail.com>
2020-03-07 16:57:53 +0900

    Fixed #9

--- model_field_meta/fields.py
+++ model_field_meta/fields.py
@@ -1,9 +1,14 @@
 class FieldWithMeta:
-    def __init__(self, **kwargs):
-        if 'meta' in kwargs:
-            self._meta = kwargs['meta']
-            del kwargs["meta"]
-        super(self.__class__, self).__init__(**kwargs)
+    @staticmethod
+    def generate___init__(base_class):
+        # Fix #9: Support custom field
+        def __init__(self, **kwargs):
+            if 'meta' in kwargs:
+                self._meta = kwargs['meta']
+                del kwargs["meta"]
+            super(base_class, self).__init__(**kwargs)
+
+        return __init__
 
     def get_meta(self):
         return self._meta
